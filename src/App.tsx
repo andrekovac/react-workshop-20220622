42f08beeaedf58cadc682694eb5f153ded06c8f4
André Kovac <me@andrekovac.com>
2022-06-24 12:21:59 +0200

    Create Add2 component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import SimpleName from "./components/SimpleName";
 
 const meinName = "Andre";
@@ -9,9 +10,48 @@ const HelloWorld: React.FC = () => {
   return <h1>Hello World {meinName}</h1>;
 };
 
+// let number = 2;
+
+// const useState = (initialValue: number) => {
+//   const obj = {
+//     value: initialValue,
+//   }
+//   const setValue = (newValue: number) => {
+//     obj.value = newValue;
+//   }
+//   return [obj.value, setValue];
+// }
+
+const Add2: React.FC = () => {
+  console.log("render");
+  const [number, setNumber] = useState<number>(5); // number bekommt initialen Wert 5
+  const [zahl, setZahl] = useState(10); // number bekommt initialen Wert 10
+
+  // const state = useState(5);
+  // const number = state[0]
+  // const setNumber = state[1]
+
+  return (
+    <div style={{ padding: 10 }}>
+      <button
+        onClick={() => {
+          setNumber(number + 2);
+          setZahl(zahl + 10);
+          console.log({ number });
+        }}
+      >
+        Add 2
+      </button>
+      <div>Number: {number}</div>
+      <div>Zahl: {zahl}</div>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <>
+      <Add2 />
       <SimpleName />
       <HelloWorld />
     </>
