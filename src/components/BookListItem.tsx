4684934244ad40c5ea43fa773ccc067e71b3314e
André Kovac <me@andrekovac.com>
2022-06-27 17:41:45 +0200

    add price

--- src/components/BookListItem.tsx
+++ src/components/BookListItem.tsx
@@ -7,7 +7,11 @@ interface BookListItemProps {
 export const BookListItem: React.FC<BookListItemProps> = ({ book }) => (
   <>
     <p>{book.title}</p>
+
     <img width="100px" src={book.cover} alt="cover"></img>
+    <span style={{ color: "red", fontStyle: "italic" }}>
+      (only {book.price}) BUY NOW!
+    </span>
   </>
 );
 
